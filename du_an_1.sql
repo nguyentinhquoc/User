3bb25e3d5c11b0ee7067f2ee7a45d7336e4279de
nguyenquoctinh <nguyentinh140321@gmail.com>
2023-11-13 12:10:55 +0700

    upload_database

---
+++ du_an_1.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2023 at 06:09 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `du_an_1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bienthesp`
+--
+-- Error reading structure for table du_an_1.bienthesp: #1932 - Table &#039;du_an_1.bienthesp&#039; doesn&#039;t exist in engine
+-- Error reading data for table du_an_1.bienthesp: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `du_an_1`.`bienthesp`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `binhluan`
+--
+-- Error reading structure for table du_an_1.binhluan: #1932 - Table &#039;du_an_1.binhluan&#039; doesn&#039;t exist in engine
+-- Error reading data for table du_an_1.binhluan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `du_an_1`.`binhluan`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chitietdh`
+--
+-- Error reading structure for table du_an_1.chitietdh: #1932 - Table &#039;du_an_1.chitietdh&#039; doesn&#039;t exist in engine
+-- Error reading data for table du_an_1.chitietdh: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `du_an_1`.`chitietdh`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `color`
+--
+-- Error reading structure for table du_an_1.color: #1932 - Table &#039;du_an_1.color&#039; doesn&#039;t exist in engine
+-- Error reading data for table du_an_1.color: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `du_an_1`.`color`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `danhmuc`
+--
+-- Error reading structure for table du_an_1.danhmuc: #1932 - Table &#039;du_an_1.danhmuc&#039; doesn&#039;t exist in engine
+-- Error reading data for table du_an_1.danhmuc: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `du_an_1`.`danhmuc`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sanpham`
+--
+-- Error reading structure for table du_an_1.sanpham: #1932 - Table &#039;du_an_1.sanpham&#039; doesn&#039;t exist in engine
+-- Error reading data for table du_an_1.sanpham: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `du_an_1`.`sanpham`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `size`
+--
+-- Error reading structure for table du_an_1.size: #1932 - Table &#039;du_an_1.size&#039; doesn&#039;t exist in engine
+-- Error reading data for table du_an_1.size: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `du_an_1`.`size`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taikhoan`
+--
+-- Error reading structure for table du_an_1.taikhoan: #1932 - Table &#039;du_an_1.taikhoan&#039; doesn&#039;t exist in engine
+-- Error reading data for table du_an_1.taikhoan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `du_an_1`.`taikhoan`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trangthai`
+--
+-- Error reading structure for table du_an_1.trangthai: #1932 - Table &#039;du_an_1.trangthai&#039; doesn&#039;t exist in engine
+-- Error reading data for table du_an_1.trangthai: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `du_an_1`.`trangthai`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vocher`
+--
+-- Error reading structure for table du_an_1.vocher: #1932 - Table &#039;du_an_1.vocher&#039; doesn&#039;t exist in engine
+-- Error reading data for table du_an_1.vocher: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `du_an_1`.`vocher`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
